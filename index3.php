9a8cb4cc369da2e6c045de2cf565ac1f1c83ff8e
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2023-01-10 15:33:36 +0100

    Tercer Punto Validacion

---
+++ index3.php
@@ -0,0 +1,12 @@
+<?php 
+
+$chocolate1 = new Chocolate("Fondant",5,3,8,5);
+
+echo $chocolate1->$nombre;
+echo $chocolate1->$numero;
+echo $chocolate1->$precio;
+echo $chocolate1->getPorcentajeCacao();
+echo $chocolate1->getPeso();
+
+
+?>
